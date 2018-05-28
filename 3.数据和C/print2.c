1eeb132d0d929ccbcc0029c77a012bf57ba43d36
nanohaikaros <1371689491@qq.com>
2018-05-28 17:20:10 +0800

    3

--- 3.数据和C/print2.c
+++ 3.数据和C/print2.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 int main(void)
 {
-    unsigned int un = 3000000000;
-    short end = 200;
+    unsigned int un = 3000000000;       /* int为32位 */
+    short end = 200;                    /* 和short为16位的系统 */
     long big = 65537; long long verybig = 12345678908642;
     printf("un = %u and not %d\n", un, un);
     printf("end = %hd and %d\n", end, end);

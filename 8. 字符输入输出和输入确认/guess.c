091eae27070d32e63f784cb4108b8b4487d1b45e
Nanoha <1371689491@qq.com>
2019-01-09 21:04:54 +0800

    8

---
+++ 8. 字符输入输出和输入确认/guess.c
@@ -0,0 +1,36 @@
+/* guess.c -- 一个低效且错误的猜数程序 */
+#include <stdio.h>
+int main(void)
+{
+    int guess = 1;
+    printf("Pick an integer from 1 to 100. I will try to guess ");
+    printf("it.\nRespond with a y if my guess is right and with");
+    printf("\nan n if it is wrong.\n");
+    printf("Uh...is your number %d?\n", guess);
+    
+/*  while(getchar() != 'y')
+        printf("Well, then, is it %d?\n", ++guess);
+*/
+
+/*  while(getchar() != 'y')
+    {
+        printf("Well, then, is it %d?\n", ++guess);
+        while(getchar() != '\n')
+            continue;
+    }
+*/
+
+    char response;
+    while((response = getchar()) != 'y')
+    {
+        if(response == 'n')
+            printf("Well, then, is it %d?\n", ++guess);
+        else
+            printf("Sorry, I understand only y or n.\n");
+        while(getchar() != '\n')
+            continue;
+    }
+
+    printf("I know I could do it!\n");
+    return 0;
+}

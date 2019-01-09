890789d1111322a5e90580b356d825c7a72290a3
Nanoha <1371689491@qq.com>
2019-01-09 17:30:57 +0800

    7

---
+++ 7.C控制语句：分支和跳转/cypher2.c
@@ -0,0 +1,17 @@
+// cypher2.c -- 改变输入，只保留非字母字符
+#include <stdio.h>
+#include <ctype.h>
+int main(void)
+{
+    char ch;
+
+    while((ch = getchar()) != '\n')
+    {
+        if(isalpha(ch))             // 如果是一个字母
+            putchar(ch + 1);            // 这改变它
+        else                        // 否则
+            putchar(ch);            // 原样打印它
+    }
+    putchar(ch);                    // 打印换行字符
+    return 0;
+}

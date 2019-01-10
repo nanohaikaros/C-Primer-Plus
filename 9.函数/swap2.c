447466ff06d9b65f8642f5ac1246e45255e3f724
Nanoha <1371689491@qq.com>
2019-01-10 15:12:03 +0800

    9

---
+++ 9.函数/swap2.c
@@ -0,0 +1,22 @@
+/* swap2.c -- 分析sqap.c程序 */
+#include <stdio.h>
+void interchange(int u, int v);     //声明函数
+int main(void)
+{
+    int x = 5, y = 10;
+    printf("Originally x = %d and y = %d.\n", x, y);
+    interchange(x, y);
+    printf("Now x = %d and y = %d.\n", x, y);
+    return 0;
+}
+
+void interchange(int u, int v)      // 定义函数
+{
+    int temp;
+
+    printf("Originally x = %d and y = %d.\n", u, v);
+    temp = u;
+    u = v;
+    v = temp;
+    printf("Now x = %d and y = %d.\n", u, v);
+}
